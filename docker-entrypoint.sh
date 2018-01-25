e92a32f3751ca0a2c59fbe84e2620e9fd124b966
Raffael <22076060+rafffael@users.noreply.github.com>
2018-01-25 07:13:24 +0100

    Extracts ProfitTrailer.jar for update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,15 +12,12 @@ PT_START="java -jar $PT_JAR -XX:+UseConcMarkSweepGC -Xmx256m -Xms256m"
 [ -d "$PT_DIR" ] || mkdir "$PT_DIR" || {
    echo "Error: no $PT_DIR found and could not make it. Exiting."; exit -1;
 }
-[ -e $PT_JAR ] || unzip -oqd $APP $PT_ZIP $PT/${PT}.jar || {
+unzip -oqd $APP $PT_ZIP $PT/${PT}.jar || {
   echo "Error: no $PT jar found. Exiting."; exit -1;
 }
 cd $PT_DIR || {
   echo "Error: problem with $PT_DIR. Exiting."; exit -1;
 }
-
-# if we find any properties files in $PT_DIR we're going to assume it's been set up.
-# if this is not what you want, then delete all the files in the $PT_DIR and we'll overwrite then with the defaults
 
 pcnt=$(/bin/ls -1 $PT_DIR/*.properties 2>/dev/null|/usr/bin/wc -l)
 [[ $pcnt -gt 0 ]] || {
