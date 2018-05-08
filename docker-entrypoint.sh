f1ada1ce9b8569f83740c3da3a3fd59b9529557e
Raffael <22076060+rafffael@users.noreply.github.com>
2018-05-08 16:15:58 +0200

    small mistake.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,10 +10,10 @@ PT_ZIP=$BASE/${PT}-${PT_VERSION}.zip
 PT_JAR=$PT_DIR/${PT}.jar
 PT_START="java -jar $PT_JAR -XX:+UseConcMarkSweepGC -Xmx256m -Xms256m"
 
-[ -d "$PT_DIR" ] || mkdir "$PT_DIR" || {
+[ -d "$PT_DIR" ] || mkdir -p "$PT_DIR" || {
    echo "Error: no $PT_DIR found and could not make it. Exiting."; exit -1;
 }
-unzip -joqd $APP $PT_ZIP $PT-$PT_VERSION/${PT}.jar || {
+unzip -joqd $PT_DIR $PT_ZIP $PT-$PT_VERSION/${PT}.jar || {
   echo "Error: no $PT jar found. Exiting."; exit -1;
 }
 cd $PT_DIR || {
@@ -22,7 +22,7 @@ cd $PT_DIR || {
 
 pcnt=$(/bin/ls -1 $PT_DIR/*.properties 2>/dev/null|/usr/bin/wc -l)
 [[ $pcnt -gt 0 ]] || {
-  echo "No properties found, extracting..."; unzip -jo $PT_ZIP -d $APP;
+  echo "No properties found, extracting..."; unzip -jo $PT_ZIP -d $PT_DIR;
   echo "Done! Now, edit your configuration files and reload the container."
   exit -1;
 } || {
