cf5f950666f4b19bd01f27f7ae660c76d3426188
Raffael <22076060+rafffael@users.noreply.github.com>
2018-05-27 12:55:13 +0200

    Version 2.0.8

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:8
-ENV PT_DL=https://github.com/taniman/profit-trailer/releases/download/2.0.4/ProfitTrailer-2.0.4.zip
+ENV PT_DL=https://github.com/taniman/profit-trailer/releases/download/2.0.8/ProfitTrailer-2.0.8.zip
 VOLUME ["/app/ProfitTrailer"]
 ADD $PT_DL /opt
 ADD docker-entrypoint.sh /
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,26 +3,26 @@
 BASE=/opt
 APP=/app
 PT="ProfitTrailer"
-PT_VERSION="2.0.4"
+PT_VERSION="2.0.8"
 
-PT_DIR=$APP/$PT
-PT_ZIP=$BASE/${PT}-${PT_VERSION}.zip
-PT_JAR=$PT_DIR/${PT}.jar
+PT_DIR=${APP}/${PT}
+PT_ZIP=${BASE}/${PT}-${PT_VERSION}.zip
+PT_JAR=${PT_DIR}/${PT}.jar
 PT_START="java -jar $PT_JAR -XX:+UseConcMarkSweepGC -Xmx256m -Xms256m"
 
 [ -d "$PT_DIR" ] || mkdir -p "$PT_DIR" || {
    echo "Error: no $PT_DIR found and could not make it. Exiting."; exit -1;
 }
-unzip -joqd $PT_DIR $PT_ZIP $PT-$PT_VERSION/${PT}.jar || {
+unzip -joqd ${PT_DIR} ${PT_ZIP} ${PT}-${PT_VERSION}/${PT}.jar || {
   echo "Error: no $PT jar found. Exiting."; exit -1;
 }
-cd $PT_DIR || {
+cd ${PT_DIR} || {
   echo "Error: problem with $PT_DIR. Exiting."; exit -1;
 }
 
-pcnt=$(/bin/ls -1 $PT_DIR/*.properties 2>/dev/null|/usr/bin/wc -l)
-[[ $pcnt -gt 0 ]] || {
-  echo "No properties found, extracting..."; unzip -jo $PT_ZIP -d $PT_DIR;
+pcnt=$(/bin/ls -1 ${PT_DIR}/*.properties 2>/dev/null|/usr/bin/wc -l)
+[[ ${pcnt} -gt 0 ]] || {
+  echo "No properties found, extracting..."; unzip -jo ${PT_ZIP} -d ${PT_DIR};
   echo "Done! Now, edit your configuration files and reload the container."
   exit -1;
 } || {
@@ -31,4 +31,4 @@ pcnt=$(/bin/ls -1 $PT_DIR/*.properties 2>/dev/null|/usr/bin/wc -l)
 }
 
 # start it
-$PT_START
+${PT_START}
