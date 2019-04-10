6906b123859ed48eab22ff3b03d12d82920915e2
Raffael <22076060+rafffael@users.noreply.github.com>
2019-04-10 09:41:16 +0200

    Fixing java command (issue #2)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ PT="ProfitTrailer"
 PT_DIR=${APP}/${PT}
 PT_ZIP=${BASE}/${PT}-${PT_VERSION}.zip
 PT_JAR=${PT_DIR}/${PT}.jar
-PT_START="java -jar $PT_JAR -XX:+UseConcMarkSweepGC -Xmx256m -Xms256m"
+PT_START="java -Djava.net.preferIPv4Stack=true -Dsun.stdout.encoding=UTF-8 -Dio.netty.allocator.numDirectArenas=0 -Djdk.nio.maxCachedBufferSize=262144 -XX:+UseSerialGC -XX:+UseStringDeduplication -Xms64m -Xmx512m -XX:CompressedClassSpaceSize=300m -XX:MaxMetaspaceSize=128m -jar $PT_JAR"
 
 [ -d "$PT_DIR" ] || mkdir -p "$PT_DIR" || {
    echo "Error: no $PT_DIR found and could not make it. Exiting."; exit -1;
