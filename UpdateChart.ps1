6789c2dc4dc024acdf9a21f5db8f5bbf575c21de
Lucas Condé <lucasconde@gmail.com>
2021-09-23 21:23:56 -0300

    Add ps1 to update chart

---
+++ UpdateChart.ps1
@@ -0,0 +1,12 @@
+﻿param ($releaseVersion)
+git checkout main
+git pull
+rm .\.deploy\*
+helm package .\charts\profit-trailer\ --destination .deploy
+cr upload -o connde -r profit-trailer -p .\.deploy\ --token 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
+git checkout gh-pages
+cr index -i .\index.yaml -p .deploy -o connde -r profit-trailer -c https://github.com/connde/profit-trailer/charts
+git add .\index.yaml
+git commit -m 'release $($releaseVersion)'
+git push origin gh-pages
+git checkout main
