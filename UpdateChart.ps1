9dba8bf6734a0438256984965f45c4a8db8175ee
Lucas Condé <lucasconde@gmail.com>
2023-03-17 01:02:31 -0300

    Fix var name change

--- UpdateChart.ps1
+++ UpdateChart.ps1
@@ -1,10 +1,10 @@
-﻿param ($chartVersion, $token)
+﻿param ($releaseVersion, $token)
 rm .\.deploy\*
 helm package .\charts\profit-trailer\ --destination .deploy
 cr upload -o connde -r profit-trailer -p .\.deploy\ --token $token
 git checkout gh-pages
 cr index -i .\index.yaml -p .deploy -o connde -r profit-trailer -c https://github.com/connde/profit-trailer/charts
 git add .\index.yaml
-git commit -m "release $chartVersion"
+git commit -m "release $releaseVersion"
 git push origin gh-pages
 git checkout main
